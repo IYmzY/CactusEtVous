c9db7ae3f1b8663fadb0377ce2c6b06ee35cbdfe
Reda <redanabil2199@gmail.com>
2022-06-23 15:07:12 +0200

    [feature] add form for survey

--- scripts/home.js
+++ scripts/home.js
@@ -1,11 +1,31 @@
-window.onload = function() {
-   var formNewsletter = document.getElementById('mc-embedded-subscribe-form');
-    formNewsletter.addEventListener('submit', function(event) {
+let cross = document.querySelector('.cross-form')
+let modalForm = document.querySelector('.modal-form')
+let btnEnquete = document.querySelector('.btn-enquete')
+
+window.onload = function () {
+    var formNewsletter = document.getElementById('mc-embedded-subscribe-form');
+    formNewsletter.addEventListener('submit', function (event) {
         var result = document.getElementById('result_news');
         result.style.display = 'block';
         formNewsletter.style.display = 'none';
         result.innerHTML = '<strong>Success!</strong> Merci pour votre inscription.';
-        
+
     }
     );
 }
+
+cross.addEventListener('click', () => {
+    if (modalForm.style.display = "flex") {
+        modalForm.style.display = "none"
+    }
+    console.log('yo')
+})
+
+btnEnquete.addEventListener('click', () => {
+    if (modalForm.style.display = "none") {
+        modalForm.style.display = "flex"
+    }
+    console.log('yo')
+})
+
+
