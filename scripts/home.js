88a2e992ccfb5a91c6abe3f082085543974eee47
valentin <valmuller14@gmail.com>
2022-06-24 22:05:59 +0200

    mise à jour v2 LP

--- scripts/home.js
+++ scripts/home.js
@@ -2,7 +2,26 @@ let cross = document.querySelector('.cross-form')
 let modalForm = document.querySelector('.modal-form')
 let btnEnquete = document.querySelector('.btn-enquete')
 
+
 window.onload = function () {
+    // menu mobil
+    var navbar = document.querySelector('.navbar')
+    var openMenu = document.querySelector('.burger');
+    var closeMenu = document.querySelector('.close-navbar');
+    openMenu.addEventListener('click', function () {
+        this.style.display = 'none';
+        navbar.style.display = 'flex';
+        closeMenu.style.display = 'block';
+    });
+    closeMenu.addEventListener('click', function () {
+        this.style.display = 'none';
+        navbar.style.display = 'none';
+        openMenu.style.display = 'block';
+    });
+
+    let nav = document.querySelector('header')
+    let sticky = nav.offsetHeight
+
     var formNewsletter = document.getElementById('mc-embedded-subscribe-form');
     formNewsletter.addEventListener('submit', function (event) {
         var result = document.getElementById('result_news');
@@ -12,20 +31,55 @@ window.onload = function () {
 
     }
     );
+
+    window.onscroll = () => {
+
+        if (window.pageYOffset > sticky ) {
+            nav.classList.add('sticky')
+            nav.classList.remove('default')
+        }else{
+            nav.classList.remove('sticky')
+            nav.classList.add('default')
+        }
+
+        let topWindow = window.scrollY;
+        topWindow = topWindow * 3;
+        let windowHeight = window.innerHeight;
+        let position = topWindow / windowHeight;
+        position = 1 - position;
+        document.querySelector('.mouse').style.opacity = position;
+    }
+
+    $(document.body).on('touchmove', mobilScroll);
+
+    function mobilScroll() {
+        if (window.pageYOffset > sticky ) {
+            nav.classList.add('sticky')
+            nav.classList.remove('default')
+        }else{
+            nav.classList.remove('sticky')
+            nav.classList.add('default')
+        }
+
+        let topWindow = window.scrollY;
+        topWindow = topWindow * 3;
+        let windowHeight = window.innerHeight;
+        let position = topWindow / windowHeight;
+        position = 1 - position;
+        document.querySelector('.mouse').style.opacity = position;
+    }
 }
 
 cross.addEventListener('click', () => {
     if (modalForm.style.display = "flex") {
         modalForm.style.display = "none"
     }
-    console.log('yo')
 })
 
 btnEnquete.addEventListener('click', () => {
     if (modalForm.style.display = "none") {
         modalForm.style.display = "flex"
     }
-    console.log('yo')
 })
 
 
