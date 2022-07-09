8553d5f893f8349ba10a08e0a3173a933efd27cc
YmzY <redanabil2199@gmail.com>
2022-07-09 16:22:00 +0200

    [fix] viteConfig

--- vite.config.js
+++ vite.config.js
@@ -10,8 +10,7 @@ module.exports = defineConfig({
                 privacypolicy: resolve(__dirname, 'pages/privacy-policy.html'),
                 connect: resolve(__dirname, 'pages/connect.html'),
                 blog: resolve(__dirname, 'pages/blog.html'),
-                addblog: resolve(__dirname, 'pages/add-blog.html'),
-
+                addblog: resolve(__dirname, 'pages/addArticles.html'),
             }
         }
     }
