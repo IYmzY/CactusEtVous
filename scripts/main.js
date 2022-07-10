137e167cc0b5c970ac6259e6a7cdcceaa0ff029d
YmzY <redanabil2199@gmail.com>
2022-07-10 02:15:07 +0200

    [style/feature] added globe 3d with config, test of carousel & some styles

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,67 @@
 import '../styles/reset.css'
 import '../styles/global.scss'
 import '../styles/main.scss'
+import '../styles/flickity.min.css'
+
+
+
+
+const colorScale = d3.scaleOrdinal(['green']);
+const labelsTopOrientation = new Set(['Arles, Italie', 'Quito, Equateur ', 'Chichaoua, Maroc', 'Tour de la Sicile', 'Iquitos, Pérou', 'Val-Des-Près, France', 'Île de Sulawesi, Indonésie', 'Ormara, Pakistan']);
+
+const elem = document.getElementById('testimony-globe');
+const testimonyGlobe = Globe()
+    .globeImageUrl('../images/imgContainers/globe-texture.png')
+    .bumpImageUrl('../images/imgContainers/globe-bumpmap.png')
+    .backgroundColor('rgba(0,0,0,0)')
+    .width([500])
+    .showGraticules(true)
+    .showAtmosphere(false)
+    .labelText('title')
+    .labelSize(4)
+    .labelDotRadius(2)
+    .labelDotOrientation(d => labelsTopOrientation.has(d.title) ? 'top' : 'bottom')
+    .labelColor(d => colorScale(d.title))
+    .labelLabel(d => `
+    <div class="testimony-content-container">
+                    <h3>${d.title}</h3>
+                    <div class=" testimony-content">
+                        <p>${d.content}</p>
+                        <img class="quote-up" src="./images/icon/quoteUp.svg">
+                        <img class="quote-down" src="./images/icon/quoteDown.svg">
+                    </div>
+                    <span><a href="${d.url}">En savoir plus</a></span>
+                </div>
+      `)
+
+    .onLabelClick(d => window.open(d.url, '_blank'))
+    (elem)
+    // .onLabelHover(onHoverLaber())
+    ;
+
+const onHoverLaber = async () => {
+    await testimonyGlobe.pauseAnimation().then(() => {
+        setTimeout(() => {
+            testimonyGlobe.resumeAnimation()
+        }, 2000)
+    })
+}
+
+
+fetch('../globe-config/globe-testimony-info.json').then(r => r.json()).then(testimonies => {
+    testimonyGlobe.labelsData(testimonies);
+});
+testimonyGlobe.resumeAnimation()
+testimonyGlobe.controls().autoRotate = true;
+testimonyGlobe.controls().autoRotateSpeed = 0.5;
+testimonyGlobe.controls().enableZoom = false
+testimonyGlobe.controls().minPolarAngle = Math.PI / 2
+testimonyGlobe.controls().maxPolarAngle = Math.PI / 2
+
+
+
+
+
 
 
 // window.onload = function () {
