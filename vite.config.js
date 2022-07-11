3d254ee89743779c9c22bc6bf61848ecc7600e1c
valentin <valmuller14@gmail.com>
2022-07-12 00:14:10 +0200

    [fix] vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -18,6 +18,18 @@ module.exports = defineConfig({
                 addTestimonials: resolve(__dirname, 'testimonials/add/index.html'),
                 editTestimonial: resolve(__dirname, 'testimonials/edit/index.html'),
                 testimonial: resolve(__dirname, 'testimonials/history/index.html'),
+                quiz: resolve(__dirname, 'quiz/index.html'),
+                quizResult: resolve(__dirname, 'quiz/results.html'),
+                q1 : resolve(__dirname, 'quiz/q1.html'),
+                q2 : resolve(__dirname, 'quiz/q2.html'),
+                q3 : resolve(__dirname, 'quiz/q3.html'),
+                q4 : resolve(__dirname, 'quiz/q4.html'),
+                q5 : resolve(__dirname, 'quiz/q5.html'),
+                q6 : resolve(__dirname, 'quiz/q6.html'),
+                q7 : resolve(__dirname, 'quiz/q7.html'),
+                q8 : resolve(__dirname, 'quiz/q8.html'),
+                q9 : resolve(__dirname, 'quiz/q9.html'),
+                q10 : resolve(__dirname, 'quiz/q10.html'),
             }
         }
     }
